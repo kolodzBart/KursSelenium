11568cae0c48bf1981116ecc6b3f92c02f1e338f
Bartłomiej Kołodziejczyk <kolodz.bart@gmail.com>
2021-01-06 22:12:16 +0100

    Ready

--- KursSelenium/Tasks/Task5_potomkowie.cs
+++ KursSelenium/Tasks/Task5_potomkowie.cs
@@ -6,18 +6,18 @@ namespace KursSelenium.Tasks
 {
     class Task5_potomkowie
     {
-        Trzy zadania:
-        1. nowosci fuertavertura "dodaj do koszyka"
-        2. Kategoria windsurfing "domyślne sortowanie"
-        3. Dodać coś do koszyka - przejść do koszyka - "cena w górnym naglówku"
+        //Trzy zadania:
+        //1. nowosci fuertavertura "dodaj do koszyka"
+        //2. Kategoria windsurfing "domyślne sortowanie"
+        //3. Dodać coś do koszyka - przejść do koszyka - "cena w górnym naglówku"
 
-        Ad1.
-            section[aria-label = "Najnowsze Produkty"] a[data-product_id="393"]
-            section.storefront-recent-products a[data - product_id = "393"]
-        Ad2.
-            header.woocommerce-products-header+div.storefront-sorting select.orderby
-            header.woocommerce-products-header+div select  <-- szukamy najpierw headera ktorej jedną z klas bedzie .woocommerce-products-header zaznacz mi pierwszego brata takiego kóry jest divem i który ma w sobie selecta i wez zaznacz tego selecta
-        Ad3.
-            a.cart-contents>.woocommerce-Price-amount
+        //Ad1.
+        //    section[aria-label = "Najnowsze Produkty"] a[data-product_id="393"]
+        //    section.storefront-recent-products a[data - product_id = "393"]
+        //Ad2.
+        //    header.woocommerce-products-header+div.storefront-sorting select.orderby
+        //    header.woocommerce-products-header+div select  <-- szukamy najpierw headera ktorej jedną z klas bedzie .woocommerce-products-header zaznacz mi pierwszego brata takiego kóry jest divem i który ma w sobie selecta i wez zaznacz tego selecta
+        //Ad3.
+        //    a.cart-contents>.woocommerce-Price-amount
     }
 }
