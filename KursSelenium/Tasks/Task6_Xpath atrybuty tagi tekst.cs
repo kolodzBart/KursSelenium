469f9f0a4fd5324268c2274fd48ba093e83598df
Bartłomiej Kołodziejczyk <kolodz.bart@gmail.com>
2021-01-14 22:19:30 +0100

    add comment

--- KursSelenium/Tasks/Task6_Xpath atrybuty tagi tekst.cs
+++ KursSelenium/Tasks/Task6_Xpath atrybuty tagi tekst.cs
@@ -6,21 +6,21 @@ namespace KursSelenium.Tasks
 {
     class Task6_Xpath_atrybuty_tagi_tekst
     {
-        Source: https://fakestore.testelka.pl/zamowienie/
+        //Source: https://fakestore.testelka.pl/zamowienie/
 
-            1. Lokalizacja pierwszej zamówionej pozycji
-            .//*[contains(text(), 'Wspinaczka Via Ferraty')]
+        //    1. Lokalizacja pierwszej zamówionej pozycji
+        //    .//*[contains(text(), 'Wspinaczka Via Ferraty')]
             
 
-            2. Lokalizacja drugiej zamówionej pozycji
-            .//*[contains(text(), 'Wspinaczka Island Peak')]
+        //    2. Lokalizacja drugiej zamówionej pozycji
+        //    .//*[contains(text(), 'Wspinaczka Island Peak')]
             
-            3. Lokalizacja checkboxa
-            .//input[@type='checkbox' and @name='createaccount']
-            .//*[@id='createaccount']   
+        //    3. Lokalizacja checkboxa
+        //    .//input[@type='checkbox' and @name='createaccount']
+        //    .//*[@id='createaccount']   
 
-            4. Zaznaczenie dwóch pól dotyczących adresu (Ulica i ciąg dalszy)
-            .//input[contains(@autocomplete, 'address-line')]
-            .//input[starts-with(@id, 'billing_address_')]
+        //    4. Zaznaczenie dwóch pól dotyczących adresu (Ulica i ciąg dalszy)
+        //    .//input[contains(@autocomplete, 'address-line')]
+        //    .//input[starts-with(@id, 'billing_address_')]
     }
 }
